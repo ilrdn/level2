d5ef0883470c9dde724be1433dc683370d36d646
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-10-24 20:35:01 +0400

    level 2 app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,30 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
+document.addEventListener('DOMContentLoaded', function() {
+  let items = document.querySelectorAll('.item');
+  items.forEach(function(e, i) {
+      let state = e.classList;
+      e.addEventListener('mouseleave', function(e) {
+          if (state.contains("selected")) {
+              state.add("selectedOut");
+          }
+      });
+      e.addEventListener('click', function(e) {
+          if (state.contains("default")) {
+              state.remove("default");
+              state.add("selected");
+          } else if (state.contains("selected")) {
+              state.remove("selected");
+              state.add("default");
+          }
+          state.remove("selectedOut");
+      });
+  });
+}, false);
 
 export default App;
